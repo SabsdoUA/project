e4232d421c68e45b835bf4f38175c865dc8917a7
Sergio [Ɐ] <subbotinsergiy1@gmail.com>
2026-02-12 02:54:31 +0100

    Disable interactive CLI by default for bootRun stability

---
+++ src/main/java/com/Subbotin/project/bootstrap/LiteratureRecommendationRunner.java
@@ -0,0 +1,22 @@
+package com.Subbotin.project.bootstrap;
+
+import LiteratureRecommendationSystem.LegacyCliFlow;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "app.cli.enabled", havingValue = "true", matchIfMissing = false)
+public class LiteratureRecommendationRunner implements CommandLineRunner {
+
+    private final LegacyCliFlow legacyCliFlow;
+
+    public LiteratureRecommendationRunner(LegacyCliFlow legacyCliFlow) {
+        this.legacyCliFlow = legacyCliFlow;
+    }
+
+    @Override
+    public void run(String... args) {
+        legacyCliFlow.execute();
+    }
+}
--- src/test/java/com/Subbotin/project/ProjectApplicationTests.java
+++ src/test/java/com/Subbotin/project/ProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.Subbotin.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "app.cli.enabled=false")
 class ProjectApplicationTests {
 
 	@Test
